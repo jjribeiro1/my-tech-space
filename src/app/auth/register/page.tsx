667552d0b7d7da8a6ffeb813bd522a4ce25d2527
jjribeiro1 <jvsr.99@hotmail.com>
2025-03-28 23:19:59 -0300

    AuthLayout component

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,9 +1,5 @@
 import RegisterForm from "@/features/auth/components/register-form";
 
 export default function RegisterPage() {
-  return (
-    <main className="flex min-h-screen items-center justify-center p-6">
-      <RegisterForm />
-    </main>
-  );
+  return <RegisterForm />;
 }
