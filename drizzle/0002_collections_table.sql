131a750a149e2ff86b4afeb4581330dd71fd0285
jjribeiro1 <jvsr.99@hotmail.com>
2025-03-29 20:05:27 -0300

    add collections table and schema definition

---
+++ drizzle/0002_collections_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE "collections" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"name" text NOT NULL,
+	"slug" text NOT NULL,
+	"description" text,
+	"is_private" boolean DEFAULT false,
+	"user_id" text,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"updated_at" timestamp with time zone,
+	"deleted_at" timestamp with time zone
+);
+--> statement-breakpoint
+ALTER TABLE "collections" ADD CONSTRAINT "collections_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;
---
+++ src/db/schema/collection.ts
@@ -0,0 +1,13 @@
+import * as t from "drizzle-orm/pg-core";
+import { timestamps } from "../helper";
+import { users } from "./users";
+
+export const collections = t.pgTable("collections", {
+  id: t.uuid().primaryKey().defaultRandom(),
+  name: t.text().notNull(),
+  slug: t.text().notNull(),
+  description: t.text(),
+  isPrivate: t.boolean().default(false),
+  userId: t.text().references(() => users.id),
+  ...timestamps,
+});
