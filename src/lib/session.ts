d1a7b82263597538b4735b1d99753976b1fa53fd
jjribeiro1 <jvsr.99@hotmail.com>
2025-03-29 20:33:01 -0300

    add getSession from server

---
+++ src/lib/session.ts
@@ -0,0 +1,11 @@
+import "server-only";
+import { headers } from "next/headers";
+import { auth } from "@/lib/auth";
+
+export async function getSession() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  return session;
+}
