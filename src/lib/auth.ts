78fa9acce5441f0022a5ebcbb0515cc89b34b0cd
jjribeiro1 <jvsr.99@hotmail.com>
2025-03-29 11:04:11 -0300

    add nextCookies plugin to betterAuth for cookie management

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,6 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { nextCookies } from "better-auth/next-js";
 import { db } from "@/db";
 import { users } from "@/db/schema/users";
 import { accounts } from "@/db/schema/account";
@@ -45,4 +46,5 @@ export const auth = betterAuth({
     enabled: true,
     minPasswordLength: 6,
   },
+  plugins: [nextCookies()]
 });
