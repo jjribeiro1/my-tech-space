0e48f1c77286d96ca03d19f471b1cdb7c6e09265
jjribeiro1 <jvsr.99@hotmail.com>
2025-04-06 19:50:11 -0300

    create resource action

---
+++ src/features/resources/actions/create-resource-action.ts
@@ -0,0 +1,55 @@
+"use server";
+import "server-only";
+import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
+import { z } from "zod";
+import { db } from "@/db";
+import { resources } from "@/db/schema/resource";
+import { getSession } from "@/lib/session";
+import { ActionResponse } from "@/types/action";
+
+const schema = z.object({
+  url: z.string().url({ message: "Invalid url" }),
+  title: z.string().min(1, { message: "Title is required" }),
+  description: z.string(),
+  collectionId: z.string().uuid(),
+  resourceTypeId: z.string().uuid(),
+});
+
+type Input = z.infer<typeof schema>;
+
+export async function createResourceAction(
+  data: Input,
+): Promise<ActionResponse> {
+  try {
+    const session = await getSession();
+    if (!session) {
+      redirect("/auth/login");
+    }
+    const validatedData = schema.safeParse(data);
+
+    if (!validatedData.success) {
+      return {
+        success: false,
+        message: "Something went wrong, invalid data",
+      };
+    }
+
+    await db.insert(resources).values({
+      ...validatedData.data,
+    });
+
+    revalidatePath("/collection", "page");
+
+    return {
+      success: true,
+      message: "Resource created successfully",
+    };
+  } catch (err) {
+    console.error(err);
+    return {
+      success: false,
+      message: "Something went wrong, please try again",
+    };
+  }
+}
