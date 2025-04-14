7b07dfe94afc6fea66b3fc7841585eecbcb93dc5
jjribeiro1 <jvsr.99@hotmail.com>
2025-04-14 18:11:25 -0300

    enhance Toaster component with closeButton and richColors props

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         {children}
-        <Toaster />
+        <Toaster closeButton richColors/>
       </body>
     </html>
   );
