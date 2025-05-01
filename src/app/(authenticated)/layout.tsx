f22cf147b92ec907ab4cc22c3c68571cd383024a
jjribeiro1 <jvsr.99@hotmail.com>
2025-05-01 13:36:00 -0300

    add UserProfile component with logout functionality

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { UserProfile } from "@/features/auth/components/user-profile";
 
 export default function AuthenticatedLayout({
   children,
@@ -9,14 +10,14 @@ export default function AuthenticatedLayout({
     <>
       <header className="bg-background/95 supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50 w-full border-b backdrop-blur">
         <div className="container mx-auto flex h-16 items-center justify-between px-4 md:px-0">
-          <div className="flex items-center gap-2">
-            <Link
-              href={"/dashboard"}
-              className="text-primary cursor-pointer font-bold md:text-xl"
-            >
-              MyTechSpace
-            </Link>
-          </div>
+          <Link
+            href={"/dashboard"}
+            className="text-primary cursor-pointer font-bold md:text-xl"
+          >
+            MyTechSpace
+          </Link>
+
+          <UserProfile />
         </div>
       </header>
       <main className="px-4 md:px-0">{children}</main>
---
+++ src/features/auth/components/user-profile.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { authClient } from "@/lib/auth-client";
+
+export function UserProfile() {
+  const router = useRouter();
+  const { data: session } = authClient.useSession();
+
+  async function logout() {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/");
+        },
+      },
+    });
+  }
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Avatar className="cursor-pointer">
+          <AvatarImage src={session?.user.image ?? ""} />
+          <AvatarFallback>A</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem className="cursor-pointer" onClick={logout}>Logout</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
