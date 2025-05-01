2360534e03575ba469f258d2b9ade9bf44cf6568
jjribeiro1 <jvsr.99@hotmail.com>
2025-05-01 12:31:05 -0300

    replace static title with Link component for navigation to dashboard

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function AuthenticatedLayout({
   children,
 }: {
@@ -8,7 +10,12 @@ export default function AuthenticatedLayout({
       <header className="bg-background/95 supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50 w-full border-b backdrop-blur">
         <div className="container mx-auto flex h-16 items-center justify-between px-4 md:px-0">
           <div className="flex items-center gap-2">
-            <div className="text-primary font-bold md:text-xl">MyTechSpace</div>
+            <Link
+              href={"/dashboard"}
+              className="text-primary cursor-pointer font-bold md:text-xl"
+            >
+              MyTechSpace
+            </Link>
           </div>
         </div>
       </header>
