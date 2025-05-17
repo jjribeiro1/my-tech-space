4247a64437a87afe86ed1d99a50fb6e5684ccc2a
jjribeiro1 <jvsr.99@hotmail.com>
2025-05-17 19:15:49 -0300

    implement debounced search input functionality

--- src/features/resources/components/search-resource-input.tsx
+++ src/features/resources/components/search-resource-input.tsx
@@ -2,21 +2,28 @@
 import { Search } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { useQueryParams } from "@/hooks/use-query-params";
+import { useEffect, useState } from "react";
+import { useDebounce } from "@/hooks/use-debounce";
 
 export function SearchResourceInput() {
   const { setQueryParam, removeQueryParam } = useQueryParams();
+  const [inputValue, setInputValue] = useState("");
+  const debouncedValue = useDebounce(inputValue, 500);
+
+  useEffect(() => {
+    if (debouncedValue === "") {
+      removeQueryParam("search");
+    } else {
+      setQueryParam("search", debouncedValue);
+    }
+  }, [debouncedValue, setQueryParam, removeQueryParam]);
 
   return (
     <div className="relative max-w-md flex-grow">
       <Search className="text-muted-foreground absolute top-1/2 left-2.5 h-4 w-4 -translate-y-1/2" />
       <Input
-        onChange={(e) => {
-          if (e.target.value === "") {
-            removeQueryParam("search");
-            return
-          }
-          setQueryParam("search", e.target.value);
-        }}
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
         type="search"
         placeholder="Search resource..."
         className="pl-8"
