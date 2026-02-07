c5c74d307ca58f18e493ff65446dad3c1c0eb076
João Vitor <jvsr.99@hotmail.com>
2026-02-07 20:19:44 -0300

    drop table resource types

--- src/db/schema/resource-type.ts
+++
@@ -1,8 +0,0 @@
-import * as t from "drizzle-orm/pg-core";
-import { timestamps } from "../helper";
-
-export const resourceTypes = t.pgTable("resource_types", {
-  id: t.uuid().primaryKey().defaultRandom(),
-  name: t.text().notNull(),
-  ...timestamps,
-});
