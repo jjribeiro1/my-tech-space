59f582526b1c9ea5e035d442d48ae8f5df928616
João Vitor <jvsr.99@hotmail.com>
2026-02-07 12:04:56 -0300

    fix: update isPrivate field to remove default value in collection schema

--- src/features/collection/schemas/create-collection-schema.ts
+++ src/features/collection/schemas/create-collection-schema.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const collectionSchema = z.object({
   name: z.string().min(1, { message: "Name is required" }),
   description: z.string().optional(),
-  isPrivate: z.boolean().default(false)
+  isPrivate: z.boolean(),
 });
 
-export type CollectionInput = z.infer<typeof collectionSchema>
+export type CollectionInput = z.infer<typeof collectionSchema>;
