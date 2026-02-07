15699d857c091cc05fbf3f14ab576a26d0293a53
João Vitor <jvsr.99@hotmail.com>
2026-02-07 13:13:07 -0300

    fix: correct typo in header class for backdrop filter support

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -9,7 +9,7 @@ export default function AuthenticatedLayout({
 }) {
   return (
     <>
-      <header className="bg-background/95 supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50 w-full border-b px-6 backdrop-blur">
+      <header className="bg-background/95 supports-backdrop-filter:bg-background/60 sticky top-0 z-50 w-full border-b px-6 backdrop-blur">
         <div className="container mx-auto flex h-16 items-center justify-between px-4 md:px-0">
           <Link
             href={"/dashboard"}
